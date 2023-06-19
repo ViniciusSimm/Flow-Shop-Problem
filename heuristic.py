91ff938345903fe6424ed90765ff5dc1ef6665fa
Vinicius Sylvestre <vinicius.sylvestre@hypeone.com>
2023-06-19 09:26:23 -0300

    one switch heuristic

--- heuristic.py
+++ heuristic.py
@@ -4,7 +4,15 @@ from tools import Tools
 import random
 
 class Heuristic():
-    def permutation(self,list_index):
-        permutations = list(itertools.permutations(list_index))
-        return permutations
-    
+    def permutation_1t(self,list_index):
+        swaps = []
+        for i in range(len(list_index)):
+            for j in range(i+1, len(list_index)):
+                new_vector = list_index.copy()
+                new_vector[i], new_vector[j] = new_vector[j], new_vector[i]
+                swaps.append(new_vector)
+        return swaps
+
+if __name__ == "__main__":
+    heuristic = Heuristic()
+    print(heuristic.permutation_1t([1,2,3,4]))
